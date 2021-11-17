19a36f0227b1fa0901daada502b1487ee98eb050
nerdjot <njsingh3948@gmail.com>
2021-11-17 11:15:03 +0530

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './style.css';
 import Market from './Market';
+import logo from './src/logo.png';
 
 export default function App() {
   return (
     <div>
       <Market />
+      <img src={logo} alt="logo" />
     </div>
   );
 }
