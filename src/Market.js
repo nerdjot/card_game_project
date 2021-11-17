5e36d9435cb5122210d61ece9edab310ddc1fef7
nerdjot <njsingh3948@gmail.com>
2021-11-17 11:19:49 +0530

    1 file modified, 1 added, 1 deleted

--- src/Market.js
+++ src/Market.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Market.css';
+import logo from './folder/logo.png';
 const Market = () => {
   return (
     <div className="market">
       <div className="menu"></div>
+      <img />
     </div>
   );
 };
